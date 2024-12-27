448a63b572eeecfb93d7cdf370a6fa86c9278b34
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-27 18:02:16 -0300

    Created the Structure

---
+++ src/main/java/com/biblioteca/ApiBiblioteca/ApibibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.ApiBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/ApiBiblioteca/ApibibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.ApiBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
