c3c5108f3e039e93ae74a210387f008b648ab413
luan048 <luanmonteiropedrosa@gmail.com>
2024-12-29 23:33:24 -0300

    Created Post, Get and Delete mathods

---
+++ src/main/java/com/biblioteca/ApiBiblioteca/controllers/BookController.java
@@ -0,0 +1,48 @@
+package com.biblioteca.ApiBiblioteca.controllers;
+
+import com.biblioteca.ApiBiblioteca.DTO.CreateBookDTO;
+import com.biblioteca.ApiBiblioteca.entity.Book;
+import com.biblioteca.ApiBiblioteca.services.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> post(@RequestBody CreateBookDTO createBookDTO) {
+        var isbn = bookService.registerBook(createBookDTO);
+
+        return ResponseEntity.created(URI.create("/v1/createBook" + isbn.toString())).build();
+    }
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<Book> getByIsbn(@PathVariable("isbn") String id) {
+        var book = bookService.getBookByIsbn(id);
+
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        var books = bookService.getAllBooks();
+
+        return ResponseEntity.ok(books);
+    }
+
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Void> deleteById(@PathVariable("isbn") String id) {
+        bookService.deleteBook(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/biblioteca/ApiBiblioteca/services/BookService.java
+++ src/main/java/com/biblioteca/ApiBiblioteca/services/BookService.java
@@ -1,8 +1,12 @@
 package com.biblioteca.ApiBiblioteca.services;
 
+import com.biblioteca.ApiBiblioteca.DTO.CreateBookDTO;
+import com.biblioteca.ApiBiblioteca.entity.Book;
 import com.biblioteca.ApiBiblioteca.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -11,5 +15,35 @@ public class BookService {
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public UUID registerBook(CreateBookDTO createBookDTO) {
+        Book book = new Book();
+
+        book.setNameBook(createBookDTO.nameBook());
+        book.setSection(createBookDTO.section());
+        book.setAuthor(createBookDTO.author());
+        book.setPublisher(createBookDTO.publisher());
+
+        bookRepository.save(book);
+
+        return book.getIsbn();
+    }
+
+    public Optional<Book> getBookByIsbn(String isbn) {
+        return bookRepository.findById(UUID.fromString(isbn));
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void deleteBook(String isbn) {
+        var id = UUID.fromString(isbn);
+        var bookExists = bookRepository.existsById(id);
+
+        if(bookExists) {
+            bookRepository.deleteById(id);
+        }
     }
 }
